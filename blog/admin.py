5f4e88646a6607534ff4971135927b5d41d523c9
iamrf <aref.fallah@gmail.com>
2022-07-19 19:39:56 +0430

    Blog model and admin panel designed

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,48 @@
 from django.contrib import admin
+from django import forms
+from .models import Tag, Post
+from jalali_date import datetime2jalali
+from ckeditor_uploader.widgets import CKEditorUploadingWidget
 
-# Register your models here.
+# Create a form for PostAdmin fields widgets
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        widgets = {
+            'content' : CKEditorUploadingWidget(),
+        }
+        fields = '__all__'
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    form = PostForm
+    filter_horizontal = ['tags']
+    fieldsets = (
+        ('عنوان',{
+            'fields' : ('title', 'slug')
+        }),
+        ('محتوا',{
+            'fields' : ('content',)
+        }),
+        ('تگ ها',{
+            'fields' : ('tags',)
+        }),
+        ('فعال/ غیرفعال',{
+            'fields' : ('is_active',)
+        }),
+    )
+    list_display = ['title', 'is_active', 'get_created', 'get_updated']
+    ordering = ['-updated']
+
+    def get_created(self, obj):
+        return datetime2jalali(obj.created).strftime('%A %-d %B %Y ساعت %H:%M')
+    def get_updated(self, obj):
+        return datetime2jalali(obj.updated).strftime('%A %-d %B %Y ساعت %H:%M')
+ 
+    get_created.short_description = 'تاریخ ایجاد'
+    get_created.admin_order_field = 'created'
+    get_updated.short_description = 'آخرین ویرایش'
+    get_updated.admin_order_field = 'updated'
+
+
+admin.site.register(Tag)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,54 @@
 from django.db import models
+from django.utils import timezone
+from django.utils.text import slugify
+from jalali_date import datetime2jalali
 
-# Create your models here.
+class Tag(models.Model):
+    name = models.CharField(max_length=50, verbose_name='عنوان')
+    slug = models.SlugField(max_length=250, unique=True, allow_unicode=True, blank=True, verbose_name='لینک')
+    created = models.DateTimeField(auto_now_add=timezone.now)
+    updated = models.DateTimeField(auto_now=timezone.now)
+
+    class Meta:
+        verbose_name = 'تگ'
+        verbose_name_plural = 'تگ ها'
+        ordering = ['-updated']
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(
+                self.name,
+                allow_unicode=True,
+            )
+        super(Tag, self).save(*args, **kwargs)
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=250, verbose_name='عنوان')
+    slug = models.SlugField(max_length=250, unique=True, allow_unicode=True, blank=True, verbose_name='لینک')
+    content = models.TextField(verbose_name='محتوا')
+    tags = models.ManyToManyField(Tag, blank=True, verbose_name='تگ ها')
+    is_active = models.BooleanField(default=True, verbose_name='فعال')
+    created = models.DateTimeField(auto_now_add=timezone.now)
+    updated = models.DateTimeField(auto_now=timezone.now)
+
+    class Meta:
+        verbose_name = 'پست'
+        verbose_name_plural = 'پست ها'
+        ordering = ['-updated']
+
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        salt = datetime2jalali(self.updated)
+        if not self.slug:
+            self.slug = slugify(
+                self.title,
+                allow_unicode=True,
+            )
+        super(Post, self).save(*args, **kwargs)
+
